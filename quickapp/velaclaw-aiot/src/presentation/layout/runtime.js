14a5c0748314ac8f6ba064b038d3fb90960fa7fd
temlike <shy20060308@163.com>
2026-09-02 16:54:46 +0800

    feat: expose adaptive layout plans to pages

---
+++ quickapp/velaclaw-aiot/src/presentation/layout/runtime.js
@@ -0,0 +1,30 @@
+var screenProfile = require('../viewport/profile').default || require('../viewport/profile')
+var adapter = require('./adapter')
+
+function regionStyle(region) {
+  if (!region) return ''
+  return 'position: absolute; left: ' + region.left + 'px; top: ' + region.top + 'px; width: ' + region.width + 'px; height: ' + region.height + 'px;'
+}
+
+function apply(page, plan) {
+  if (!page || !plan) return
+  page.layoutPlan = plan
+  page.layoutScale = plan.scale
+  page.layoutNeedsOverride = plan.needsOverride
+  page.layoutComposition = plan.composition
+  page.layoutRegions = adapter.regionMap(plan)
+}
+
+function bind(page, spec, callback) {
+  screenProfile.resolve(page, function (profile) {
+    var plan = adapter.resolve(profile, spec)
+    apply(page, plan)
+    if (typeof callback === 'function') callback(plan, profile)
+  })
+}
+
+module.exports = {
+  bind: bind,
+  apply: apply,
+  regionStyle: regionStyle
+}
