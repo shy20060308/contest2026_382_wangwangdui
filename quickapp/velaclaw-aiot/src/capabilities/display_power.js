52f1007e71819cc10394472e262a41210f1f505b
temlike <shy20060308@163.com>
2026-09-02 18:30:44 +0800

    feat: expose brightness mode through display capability

--- quickapp/velaclaw-aiot/src/capabilities/display_power.js
+++ quickapp/velaclaw-aiot/src/capabilities/display_power.js
@@ -15,6 +15,9 @@ export default {
     var level = Math.max(0, Math.min(255, Math.round(Number(value) || 0)))
     return call(brightness && brightness.setValue, { value: level })
   },
+  setMode: function (automatic) {
+    return call(brightness && brightness.setMode, { mode: automatic ? 1 : 0 })
+  },
   setKeepScreenOn: function (keepScreenOn) {
     return call(brightness && brightness.setKeepScreenOn, { keepScreenOn: !!keepScreenOn })
   }
