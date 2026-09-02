e85233813ee4458f23ee80a246354ac935dccc12
temlike <shy20060308@163.com>
2026-09-02 17:34:28 +0800

    refactor: make clock health bridge consume heart-rate capability

--- quickapp/velaclaw-aiot/src/common/health_sample_service.js
+++ quickapp/velaclaw-aiot/src/common/health_sample_service.js
@@ -1,3 +1,40 @@
-import healthPlatform from '../platform/vela/health'
+import heartRate from '../capabilities/heart_rate'
 
-export default healthPlatform
+var entries = []
+
+function mapSnapshot(snapshot) {
+  var source = snapshot || heartRate.getSnapshot()
+  return {
+    heartRate: source.value,
+    heartRateLive: source.live,
+    heartRateUpdatedAt: source.updatedAt || 0,
+    heartRateErrorCode: source.errorCode || 0,
+    anyLive: !!source.live,
+    serviceAvailable: !!source.available,
+    sourceText: source.live ? '系统实时数据' : '兼容演示数据',
+    updatedAt: source.updatedAt || 0
+  }
+}
+
+function start(listener) {
+  if (typeof listener !== 'function') return
+  for (var i = 0; i < entries.length; i++) if (entries[i].listener === listener) return
+  var wrapped = function (snapshot) { listener(mapSnapshot(snapshot)) }
+  entries.push({ listener: listener, wrapped: wrapped })
+  heartRate.subscribe(wrapped)
+}
+
+function stop(listener) {
+  var next = []
+  for (var i = 0; i < entries.length; i++) {
+    if (entries[i].listener === listener) heartRate.unsubscribe(entries[i].wrapped)
+    else next.push(entries[i])
+  }
+  entries = next
+}
+
+export default {
+  start: start,
+  stop: stop,
+  getSnapshot: function () { return mapSnapshot(heartRate.getSnapshot()) }
+}
