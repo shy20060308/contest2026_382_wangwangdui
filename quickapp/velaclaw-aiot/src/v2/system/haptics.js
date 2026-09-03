4ac2dfb29f42263b5809d22ea3ef4fbc1d4f142b
temlike <shy20060308@163.com>
2026-09-04 07:58:00 +0800

    refactor: arbitrate haptics by owner

--- quickapp/velaclaw-aiot/src/v2/system/haptics.js
+++ quickapp/velaclaw-aiot/src/v2/system/haptics.js
@@ -1,25 +1,21 @@
 import vibration from '../../capabilities/vibration'
 var patterns = require('../../domain/haptics/patterns')
+var core = require('./haptics_core')
 
-var timers = []
+var runtime = core.createRuntime({
+  vibrate: function (mode) { return vibration.vibrate(mode) }
+})
 
-function clearTimers() {
-  for (var i = 0; i < timers.length; i++) clearTimeout(timers[i])
-  timers = []
+function play(pattern, level, owner) {
+  return runtime.play(patterns.get(pattern, level), owner)
 }
 
-function play(pattern, level) {
-  clearTimers()
-  var spec = patterns.get(pattern, level)
-  vibration.vibrate(spec.mode)
-  for (var i = 1; i < spec.count; i++) {
-    ;(function (delay, mode) {
-      timers.push(setTimeout(function () { vibration.vibrate(mode) }, delay))
-    })(i * (spec.duration + spec.interval), spec.mode)
-  }
+function stop(owner) {
+  return runtime.stop(owner)
 }
 
 export default {
   play: play,
-  stop: clearTimers
+  stop: stop,
+  getActiveOwner: runtime.getActiveOwner
 }
