30eef0c095fc2d8828ce5ac50ed5fc22a3016077
temlike <shy20060308@163.com>
2026-09-03 22:10:41 +0800

    rewrite: make system event a lazy capability gateway

--- quickapp/velaclaw-aiot/src/capabilities/system_event.js
+++ quickapp/velaclaw-aiot/src/capabilities/system_event.js
@@ -1,7 +1,45 @@
 import systemEvent from '@system.event'
 
-export default {
-  isAvailable: function () {
-    return !!(systemEvent && systemEvent.subscribe)
+var subscriptions = []
+
+function isAvailable() {
+  return !!(systemEvent && systemEvent.subscribe)
+}
+
+function subscribe(eventName, listener) {
+  if (!eventName || typeof listener !== 'function') return false
+  for (var i = 0; i < subscriptions.length; i++) {
+    if (subscriptions[i].eventName === eventName && subscriptions[i].listener === listener) return true
   }
+  if (!isAvailable()) return false
+  try {
+    var id = systemEvent.subscribe({ eventName: eventName, callback: listener })
+    if (id === undefined || id === null) return false
+    subscriptions.push({ eventName: eventName, listener: listener, id: id })
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+function unsubscribe(eventName, listener) {
+  var next = []
+  for (var i = 0; i < subscriptions.length; i++) {
+    var item = subscriptions[i]
+    if (item.eventName === eventName && item.listener === listener) {
+      try {
+        if (systemEvent && systemEvent.unsubscribe) systemEvent.unsubscribe({ id: item.id })
+      } catch (error) {}
+    } else {
+      next.push(item)
+    }
+  }
+  subscriptions = next
+}
+
+export default {
+  isAvailable: isAvailable,
+  subscribe: subscribe,
+  unsubscribe: unsubscribe,
+  consumerCount: function () { return subscriptions.length }
 }
