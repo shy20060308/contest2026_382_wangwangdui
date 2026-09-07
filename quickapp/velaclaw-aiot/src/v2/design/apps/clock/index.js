20ebe7bcbf115f6ddad493c5de7019285048a60c
temlike <shy20060308@163.com>
2026-09-07 16:46:15 +0800

    refactor(v3): make Clock resolver a pure recipe projection

--- quickapp/velaclaw-aiot/src/v2/design/apps/clock/index.js
+++ quickapp/velaclaw-aiot/src/v2/design/apps/clock/index.js
@@ -2,17 +2,13 @@ var freedom = require('../../freedom')
 var adapter = require('../../adapter')
 var layout = require('./layout')
 
-function px(value) { return Number(value) + 'px' }
-
 function resolve(profile, scene, safe) {
   var config = adapter.select(layout, profile)
   var plan = adapter.createPlan(profile, scene, safe, freedom.FREE, config.surface)
   plan.faceIds = config.faceIds.slice()
   plan.notificationOverlay = !!config.notificationOverlay
   plan.faces = config.faces
-  plan.alpineDataGlassTop = px(config.alpine.dataGlassTop)
-  plan.alpineDataRowTop = px(config.alpine.dataRowTop)
-  plan.alpineBatteryRowTop = px(config.alpine.batteryTop)
+  plan.chrome = config.chrome
   return plan
 }
 
