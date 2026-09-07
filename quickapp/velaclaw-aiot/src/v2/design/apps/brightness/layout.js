dc30a497edc7a9af71e3f77c4a9eeb765b24ff71
temlike <shy20060308@163.com>
2026-09-07 17:26:59 +0800

    refactor(v3): remove unused brightness compatibility fields

--- quickapp/velaclaw-aiot/src/v2/design/apps/brightness/layout.js
+++ quickapp/velaclaw-aiot/src/v2/design/apps/brightness/layout.js
@@ -7,23 +7,10 @@ module.exports = {
     headerHeight: 24,
     headerGap: 8,
     streamWidth: 164,
-    compact: false,
-    tall: false,
     titleSize: 12,
     cardRadius: 16,
     cardGap: 6,
-    bodySize: 7,
     valueSize: 18,
-    testTitleSize: 10,
-    controls: {
-      statusCardHeight: 64,
-      levelRowHeight: 34,
-      levelButtonHeight: 34,
-      testCardHeight: 56,
-      capabilityCardHeight: 64,
-      patternCardHeight: 50,
-      pagerHeight: 30
-    },
     chrome: {
       backSize: 8,
       cardPadding: 12,
@@ -46,23 +33,10 @@ module.exports = {
     headerHeight: 20,
     headerGap: 5,
     streamWidth: 148,
-    compact: true,
-    tall: false,
     titleSize: 12,
     cardRadius: 14,
     cardGap: 6,
-    bodySize: 7,
-    valueSize: 18,
-    testTitleSize: 10,
-    controls: {
-      statusCardHeight: 64,
-      levelRowHeight: 34,
-      levelButtonHeight: 34,
-      testCardHeight: 56,
-      capabilityCardHeight: 64,
-      patternCardHeight: 50,
-      pagerHeight: 18
-    }
+    valueSize: 18
   },
   pill: {
     contentWidth: 168,
@@ -71,25 +45,10 @@ module.exports = {
     headerHeight: 32,
     headerGap: 8,
     streamWidth: 168,
-    compact: false,
-    tall: true,
     titleSize: 14,
     cardRadius: 20,
     cardGap: 8,
-    bodySize: 9,
-    valueSize: 22,
-    testTitleSize: 12,
-    controls: {
-      statusCardHeight: 82,
-      levelRowHeight: 44,
-      levelButtonHeight: 44,
-      testCardHeight: 70,
-      capabilityCardHeight: 82,
-      patternCardHeight: 64,
-      pagerHeight: 34
-    }
+    valueSize: 22
   },
-  rect: {
-    contentWidth: 164
-  }
+  rect: { contentWidth: 164 }
 }
