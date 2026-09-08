c82e3fa72291fda9f833e33f614b3533cb8cca1f
temlike <shy20060308@163.com>
2026-09-08 19:29:22 +0800

    refactor(v3): trust canonical Workout Selection model

--- quickapp/velaclaw-aiot/src/v2/design/apps/workout/selection_view.js
+++ quickapp/velaclaw-aiot/src/v2/design/apps/workout/selection_view.js
@@ -1,10 +1,9 @@
 var catalog = require('../../workout_catalog')
 
 function project(model) {
-  var source = model || {}
   return {
-    modes: catalog.list(source.modeTypes),
-    hasActive: !!source.hasActive
+    modes: catalog.list(model.modeTypes),
+    hasActive: model.hasActive
   }
 }
 
