da2bfa655c452c703279a5d0617d6ba89998939d
temlike <shy20060308@163.com>
2026-09-08 16:05:25 +0800

    refactor(health): remove fabricated stress samples

--- quickapp/velaclaw-aiot/src/capabilities/stress.js
+++ quickapp/velaclaw-aiot/src/capabilities/stress.js
@@ -2,10 +2,5 @@ import createHealthChannel from './internal/health_channel'
 
 export default createHealthChannel({
   dataTypeName: 'STRESS',
-  fallbackDataType: 9,
-  initialValue: 28,
-  fallbackInterval: 10000,
-  fallbackValue: function (tick) {
-    return 18 + ((tick * 5) % 28)
-  }
+  fallbackDataType: 9
 })
