f6f34606a76721614cdc2793c861210b9bc6f822
temlike <shy20060308@163.com>
2026-09-08 14:31:01 +0800

    refactor(v3): migrate notification resolver to L1 difference

--- quickapp/velaclaw-aiot/src/v2/design/apps/notification/index.js
+++ quickapp/velaclaw-aiot/src/v2/design/apps/notification/index.js
@@ -1,4 +1,4 @@
-var freedom = require('../../freedom')
+var difference = require('../../difference')
 var adapter = require('../../adapter')
 var layout = require('./layout')
 
@@ -6,7 +6,7 @@ function contentWidth(profile) { return adapter.contentWidth(profile, layout) }
 
 function resolve(profile, scene, safe) {
   var config = adapter.select(layout, profile)
-  var plan = adapter.createPlan(profile, scene, safe, freedom.AUTO, config.surface)
+  var plan = adapter.createPlan(profile, scene, safe, difference.L1, config.surface)
   plan.title = adapter.placeBand(profile, scene, safe, config.title)
   plan.buttons = adapter.placeBand(profile, scene, safe, config.buttons)
   plan.buttonHeight = config.buttonHeight
@@ -18,4 +18,4 @@ function resolve(profile, scene, safe) {
   return plan
 }
 
-module.exports = { freedomLevel: freedom.AUTO, contentWidth: contentWidth, resolve: resolve, layout: layout }
+module.exports = { differenceLevel: difference.L1, contentWidth: contentWidth, resolve: resolve, layout: layout }
