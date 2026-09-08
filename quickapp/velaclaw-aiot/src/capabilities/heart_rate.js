32565d26fb1082cbe8e6976a50696af798daedfc
temlike <shy20060308@163.com>
2026-09-08 18:59:51 +0800

    refactor(v3): remove heart-rate datatype fallback

--- quickapp/velaclaw-aiot/src/capabilities/heart_rate.js
+++ quickapp/velaclaw-aiot/src/capabilities/heart_rate.js
@@ -1,6 +1,3 @@
 import createHealthChannel from './internal/health_channel'
 
-export default createHealthChannel({
-  dataTypeName: 'HEART_RATE',
-  fallbackDataType: 0
-})
+export default createHealthChannel({ dataTypeName: 'HEART_RATE' })
