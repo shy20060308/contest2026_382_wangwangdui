0790386d6969a824f755e08f97552453ff8cf45e
temlike <shy20060308@163.com>
2026-09-08 16:05:03 +0800

    refactor(health): remove fabricated heart-rate samples

--- quickapp/velaclaw-aiot/src/capabilities/heart_rate.js
+++ quickapp/velaclaw-aiot/src/capabilities/heart_rate.js
@@ -2,10 +2,5 @@ import createHealthChannel from './internal/health_channel'
 
 export default createHealthChannel({
   dataTypeName: 'HEART_RATE',
-  fallbackDataType: 0,
-  initialValue: 88,
-  fallbackInterval: 3000,
-  fallbackValue: function (tick) {
-    return 78 + ((tick * 7) % 19)
-  }
+  fallbackDataType: 0
 })
