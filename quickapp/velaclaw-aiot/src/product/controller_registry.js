5f59547f5cbd5aa6e111b0364e001f4b4cc3f827
temlike <shy20060308@163.com>
2026-09-10 22:22:04 +0800

    feat(v3): register history and health surface controllers

--- quickapp/velaclaw-aiot/src/product/controller_registry.js
+++ quickapp/velaclaw-aiot/src/product/controller_registry.js
@@ -1,4 +1,6 @@
 import { createActivityController } from './features/activity/controller'
+import { createHistoryController } from './features/history/controller'
+import { createHealthController } from './features/health/controller'
 
 function noop() {}
 
@@ -14,8 +16,34 @@ function activity(onChange) {
   }
 }
 
+function history(onChange) {
+  var controller = createHistoryController(function (model) {
+    if (typeof onChange === 'function') onChange(model || {})
+  })
+  return {
+    start: function () { controller.load() },
+    stop: noop,
+    destroy: noop,
+    action: noop
+  }
+}
+
+function health(onChange) {
+  var controller = createHealthController(function (model) {
+    if (typeof onChange === 'function') onChange(model || {})
+  })
+  return {
+    start: function () { controller.start() },
+    stop: function () { controller.stop() },
+    destroy: function () { controller.stop() },
+    action: noop
+  }
+}
+
 function create(id, onChange) {
   if (id === 'activity') return activity(onChange)
+  if (id === 'history') return history(onChange)
+  if (id === 'health') return health(onChange)
   if (id === null || id === undefined || id === '') return { start: noop, stop: noop, destroy: noop, action: noop }
   throw new Error('Unknown V3 surface controller: ' + id)
 }
