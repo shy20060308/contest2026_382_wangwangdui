19ed4cbef7da67e1da9148b13f78cc2c82356caa
temlike <shy20060308@163.com>
2026-09-12 13:04:38 +0800

    fix(v3): defer activity feature module execution

--- quickapp/velaclaw-aiot/src/product/controller_bindings/activity.js
+++ quickapp/velaclaw-aiot/src/product/controller_bindings/activity.js
@@ -1,7 +1,13 @@
-import { createActivityController } from '../features/activity/controller'
 import { noop } from './shared'
 
+function activityControllerFactory() {
+  var feature = require('../features/activity/controller')
+  if (!feature || typeof feature.createActivityController !== 'function') throw new Error('Activity feature controller module unavailable')
+  return feature.createActivityController
+}
+
 function create(onChange) {
+  var createActivityController = activityControllerFactory()
   var controller = createActivityController(function (metrics) { if (typeof onChange === 'function') onChange({ metrics: metrics }) })
   return { start: function () { controller.start() }, stop: function () { controller.stop() }, destroy: function () { controller.stop() }, action: noop }
 }
