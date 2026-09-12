7a22a9070315473c49eb098601d01d02d196ced2
temlike <shy20060308@163.com>
2026-09-12 13:04:23 +0800

    fix(v3): defer health feature module execution

--- quickapp/velaclaw-aiot/src/product/controller_bindings/health.js
+++ quickapp/velaclaw-aiot/src/product/controller_bindings/health.js
@@ -1,7 +1,13 @@
-import { createHealthController } from '../features/health/controller'
 import { noop } from './shared'
 
+function healthControllerFactory() {
+  var feature = require('../features/health/controller')
+  if (!feature || typeof feature.createHealthController !== 'function') throw new Error('Health feature controller module unavailable')
+  return feature.createHealthController
+}
+
 function create(onChange) {
+  var createHealthController = healthControllerFactory()
   var controller = createHealthController(function (model) { if (typeof onChange === 'function') onChange(model || {}) })
   return { start: function () { controller.start() }, stop: function () { controller.stop() }, destroy: function () { controller.stop() }, action: noop }
 }
