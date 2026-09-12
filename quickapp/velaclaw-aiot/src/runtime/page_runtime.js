1bd341feb04422228c909f0cb770cf891824e488
temlike <shy20060308@163.com>
2026-09-12 12:22:58 +0800

    debug(v3): propagate page-runtime boot failures

--- quickapp/velaclaw-aiot/src/runtime/page_runtime.js
+++ quickapp/velaclaw-aiot/src/runtime/page_runtime.js
@@ -1,26 +1,34 @@
 import deviceProfile from './device_profile'
 var scene = require('../product/design/scene')
 
-function bind(page, callback, isCurrent) {
+function bind(page, callback, isCurrent, onError) {
   if (!page) throw new Error('V3 Page Runtime requires a page instance')
   if (typeof callback !== 'function') throw new Error('V3 Page Runtime requires a resolve callback')
+  function fail(error) {
+    if (typeof isCurrent === 'function' && !isCurrent()) return
+    if (typeof onError === 'function') onError(error)
+  }
   deviceProfile.resolve(page, function (profile) {
     if (typeof isCurrent === 'function' && !isCurrent()) return
-    var host = scene.resolve(profile)
-    var safe = scene.safe(profile, host)
-    if (typeof isCurrent === 'function' && !isCurrent()) return
+    try {
+      var host = scene.resolve(profile)
+      var safe = scene.safe(profile, host)
+      if (typeof isCurrent === 'function' && !isCurrent()) return
 
-    page.viewportClass = ''
-    page.viewportPosition = 'absolute'
-    page.viewportLeft = '0px'
-    page.viewportTop = '0px'
-    page.viewportWidth = host.width + 'px'
-    page.viewportHeight = host.height + 'px'
-    page.sceneWidth = host.width
-    page.sceneHeight = host.height
+      page.viewportClass = ''
+      page.viewportPosition = 'absolute'
+      page.viewportLeft = '0px'
+      page.viewportTop = '0px'
+      page.viewportWidth = host.width + 'px'
+      page.viewportHeight = host.height + 'px'
+      page.sceneWidth = host.width
+      page.sceneHeight = host.height
 
-    callback(profile, host, safe)
-  })
+      callback(profile, host, safe)
+    } catch (error) {
+      fail(error)
+    }
+  }, fail)
 }
 
 export default { bind: bind }
