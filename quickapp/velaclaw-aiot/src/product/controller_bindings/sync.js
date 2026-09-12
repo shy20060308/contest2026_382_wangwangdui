cd8599c24e871650afb02159c2252f4995933288
temlike <shy20060308@163.com>
2026-09-12 13:05:59 +0800

    fix(v3): defer sync feature module execution

--- quickapp/velaclaw-aiot/src/product/controller_bindings/sync.js
+++ quickapp/velaclaw-aiot/src/product/controller_bindings/sync.js
@@ -1,6 +1,11 @@
-import { createSyncController } from '../features/sync/controller'
+function syncControllerFactory() {
+  var feature = require('../features/sync/controller')
+  if (!feature || typeof feature.createSyncController !== 'function') throw new Error('Sync feature controller module unavailable')
+  return feature.createSyncController
+}
 
 function create(onChange) {
+  var createSyncController = syncControllerFactory()
   var controller = createSyncController(function (model) { if (typeof onChange === 'function') onChange(model || {}) })
   return {
     start: function () { controller.load() }, stop: function () { controller.stop() }, destroy: function () { controller.stop() },
