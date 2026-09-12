c3f9b8b1411a4f41ad6e390ab70afe0c92854f47
temlike <shy20060308@163.com>
2026-09-12 13:05:03 +0800

    fix(v3): defer history feature module execution

--- quickapp/velaclaw-aiot/src/product/controller_bindings/history.js
+++ quickapp/velaclaw-aiot/src/product/controller_bindings/history.js
@@ -1,7 +1,13 @@
-import { createHistoryController } from '../features/history/controller'
 import { noop } from './shared'
 
+function historyControllerFactory() {
+  var feature = require('../features/history/controller')
+  if (!feature || typeof feature.createHistoryController !== 'function') throw new Error('History feature controller module unavailable')
+  return feature.createHistoryController
+}
+
 function create(onChange) {
+  var createHistoryController = historyControllerFactory()
   var controller = createHistoryController(function (model) { if (typeof onChange === 'function') onChange(model || {}) })
   return { start: function () { controller.load() }, stop: noop, destroy: noop, action: noop }
 }
